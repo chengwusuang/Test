30e62854823634e8e9e62e0ffdb59ea50c75b007
chengwusuang <1223908388@qq.com>
2021-09-10 17:51:41 +0800

    first commit

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("hello");
+    }
+}
